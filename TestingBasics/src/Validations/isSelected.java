1480bd6bd52d6db36d84caa96d2a3934c443c2cc
Gnanasekar RS <gnanasekargithub@gmail.com>
2018-02-19 20:24:11 +0530

    Selected

---
+++ TestingBasics/src/Validations/isSelected.java
@@ -0,0 +1,28 @@
+package Validations;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/* if the radio Button is selected
+ * then it return True 
+ * if it fails then it will send false in Boolean
+ * so far Tested in UI
+*/
+public class isSelected {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.setProperty("webdriver.chrome.driver","D:\\Sekar\\sekar Project Backups\\Escllipse\\ChromeDriver\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://www.spicejet.com/");
+        Boolean c =driver.findElement(By.xpath("//input[@id='ctl00_mainContent_rbtnl_Trip_0']")).isSelected();
+		System.out.println(c);	
+		Boolean d = driver.findElement(By.xpath("//input[@id='ctl00_mainContent_rbtnl_Trip_1']")).isSelected();
+		System.out.println(d);
+		driver.quit();
+		
+	}
+
+}
--- TestingBasics/src/buttonsANDboxes/AlertBox.java
+++ TestingBasics/src/buttonsANDboxes/AlertBox.java
@@ -1,8 +1,17 @@
 package buttonsANDboxes;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AlertBox {
 
 	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","D:\\Sekar\\sekar Project Backups\\Escllipse\\ChromeDriver\\chromedriver_win32\\chromedriver.exe");
+	    WebDriver driver = new ChromeDriver();
+	    driver.get("http://www.spicejet.com/");
+	    
+	    
 		
 	}
 
